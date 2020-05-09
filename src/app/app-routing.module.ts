d2434733b9cf5d585d7f056803bbcc7a4a7fa306
zanuka <git@zanuka.com>
2020-05-10 02:24:32 +1200

    intro guard route logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { IntroGuard } from './guards/intro.guard';
 
 const routes: Routes = [
+  {
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuPageModule),
+    canActivate: [IntroGuard]
+  },
   {
     path: 'intro',
     loadChildren: () => import('./pages/intro/intro.module').then(m => m.IntroPageModule)
   },
   {
-    path: 'menu',
-    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
-  },
+    path: '',
+    redirectTo: 'menu',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
--- src/app/guards/intro.guard.ts
+++ src/app/guards/intro.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IntroGuard implements CanActivate {
-  canActivate(
+
+  constructor(private storage: Storage, private router: Router) { }
+
+  async canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): Promise<boolean> {
+    const introSeen = await this.storage.get('seen-intro');
+
+    if (!introSeen) {
+      this.router.navigateByUrl('/intro');
+    }
+
+    return introSeen;
   }
 
 }
